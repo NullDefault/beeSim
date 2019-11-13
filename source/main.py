de45a63a8443306300e507acad24550c4d5fe625
NullDefault <davisha999@gmail.com>
2019-11-13 13:50:11 -0500

    removed magic numbers from main, moving them to class fields

--- source/main.py
+++ source/main.py
@@ -17,11 +17,19 @@ _background = background.Background((0, 0))  # This is just the background
 _hives = []
 _hiveNumber = 6
 used_coordinates = []
+_hive_spawn_range = (60, 940)
+acceptable_hive_distance = 75
 
 _bees = []
 _beesPerHive = 10
+_bee_spawn_offset = (-30, 30)
+
+
 _flowers = []
-_flowerNumber = 200
+_initialFlowerBeds = 10
+_flowers_per_bed = 10
+_flower_spawn_range = (0, 1000)
+_flower_bed_spawning_offset = (-30, 30)
 
 
 ########################################################################################################################
@@ -32,12 +40,12 @@ def spawn_hives(number_of_hives, bees_per_hive):
 
         done = False
         while not done:
-            x_hive_coordinate = random.randint(60, 940)
-            y_hive_coordinate = random.randint(60, 940)
+            x_hive_coordinate = random.randint(_hive_spawn_range[0], _hive_spawn_range[1])
+            y_hive_coordinate = random.randint(_hive_spawn_range[0], _hive_spawn_range[1])
             done = True
             for n in used_coordinates:
-                distance = abs(math.sqrt( pow(n[0] - x_hive_coordinate, 2) + pow(n[1] - y_hive_coordinate, 2)))
-                if distance < 75:
+                distance = abs(math.sqrt(pow(n[0] - x_hive_coordinate, 2) + pow(n[1] - y_hive_coordinate, 2)))
+                if distance < acceptable_hive_distance:
                     done = False
 
             used_coordinates.append((x_hive_coordinate, y_hive_coordinate))
@@ -47,16 +55,25 @@ def spawn_hives(number_of_hives, bees_per_hive):
         _hives.append(new_hive)
 
         for j in range(bees_per_hive):
-            new_bee = beeData.Bee((x_hive_coordinate+random.randint(-30, 30),
-                                   y_hive_coordinate+random.randint(-30, 30)), new_hive)
+            new_bee = beeData.Bee((x_hive_coordinate+random.randint(_bee_spawn_offset[0], _bee_spawn_offset[1]),
+                                   y_hive_coordinate+random.randint(_bee_spawn_offset[0], _bee_spawn_offset[1])),
+                                  new_hive)
             _bees.append(new_bee)
 ########################################################################################################################
 
 
 def spawn_flowers(number_of_flowers):
     for i in range(number_of_flowers):
-        new_flower = flowerData.Flower((random.randint(0, 1000), random.randint(0, 1000)), random.randint(1, 3))
-        _flowers.append(new_flower)
+        flower_type = random.randint(1, 3)
+        flower_bed_seed = (random.randint(_flower_spawn_range[0], _flower_spawn_range[1]),
+                           random.randint(_flower_spawn_range[0], _flower_spawn_range[1]))
+        for i in range(_flowers_per_bed):
+            x_random = random.randint(_flower_bed_spawning_offset[0], _flower_bed_spawning_offset[1])
+            y_random = random.randint(_flower_bed_spawning_offset[0], _flower_bed_spawning_offset[1])
+            new_flower = flowerData.Flower((flower_bed_seed[0] + x_random, flower_bed_seed[1] + y_random)
+                                           , flower_type)
+            _flowers.append(new_flower)
+
 ########################################################################################################################
 
 
@@ -77,7 +94,7 @@ def main():
     spawn_hives(_hiveNumber, _beesPerHive)
 ########################################################################################################################
 # Spawn Flowers
-    spawn_flowers(_flowerNumber)
+    spawn_flowers(_initialFlowerBeds)
 ########################################################################################################################
 # Main Game Loop
     while True:
